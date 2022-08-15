0218fc03c2d34f70e10b229d7940667e52311b09
Christopher Alphonse <christopheralphonse96.com>
2022-08-15 14:24:20 -0400

    making changes

--- src/data.jsx
+++ src/data.jsx
@@ -17,20 +17,7 @@ import Project4 from "./assets/img/projects/p4.webp";
 import Project5 from "./assets/img/projects/p5.webp";
 import Project6 from "./assets/img/projects/p6.webp";
 
-// // skills images
-// import SkillImg1 from "./assets/img/skills/html5.png";
-// import SkillImg2 from "./assets/img/skills/css3.png";
-// import SkillImg3 from "./assets/img/skills/js.png";
-// import SkillImg4 from "./assets/img/skills/reactjs.png";
-// import SkillImg5 from "./assets/img/skills/nextjs.png";
-// import SkillImg6 from "./assets/img/skills/nodejs.png";
-// import SkillImg7 from "./assets/img/skills/git.png";
-// import SkillImg8 from "./assets/img/skills/figma.png";
 
-// testimonial images
-import TestiImage1 from "./assets/img/testimonials/testimonial-1.webp";
-import TestiImage2 from "./assets/img/testimonials/testimonial-2.webp";
-import TestiImage3 from "./assets/img/testimonials/testimonial-3.webp";
 
 // social
 export const social = [
@@ -102,34 +89,6 @@ export const projectsNav = [
   },
 ];
 
-// skill
-// export const skills = [
-//   {
-//     image: SkillImg1,
-//   },
-//   {
-//     image: SkillImg2,
-//   },
-//   {
-//     image: SkillImg3,
-//   },
-//   {
-//     image: SkillImg4,
-//   },
-//   {
-//     image: SkillImg5,
-//   },
-//   {
-//     image: SkillImg6,
-//   },
-//   {
-//     image: SkillImg7,
-//   },
-//   {
-//     image: SkillImg8,
-//   },
-// ];
-
 // services
 export const services = [
   {
@@ -152,31 +111,6 @@ export const services = [
     name: "SEO",
     description:
       "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odio perferendis volm quibusdam ullam qui dolore.",
-  },
-];
-
-// testimonials
-export const testimonials = [
-  {
-    authorImg: TestiImage1,
-    authorText:
-      "Love the simplicity of the service and the prompt customer support. We can’t imagine working without it.",
-    authorName: "Olivia Doe",
-    authorPosition: "Head of Design, Google",
-  },
-  {
-    authorImg: TestiImage2,
-    authorText:
-      "Love the simplicity of the service and the prompt customer support. We can’t imagine working without it.",
-    authorName: "Olivia Doe",
-    authorPosition: "Head of Design, Google",
-  },
-  {
-    authorImg: TestiImage3,
-    authorText:
-      "Love the simplicity of the service and the prompt customer support. We can’t imagine working without it.",
-    authorName: "Olivia Doe",
-    authorPosition: "Head of Design, Google",
   },
 ];
 
