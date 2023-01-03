66733616a7c0f9f79ee6ee19178f2ce737cd215e
Christopher Alphonse <christopheralphonse96@gmail.com>
2023-01-03 14:01:48 -0500

    aa

--- vite.config.js
+++ vite.config.js
@@ -1,25 +1,37 @@
-import * as path from "path";
-
 import { VitePWA } from "vite-plugin-pwa";
 import { defineConfig } from "vite";
-import manifest from "./manifest.json";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     VitePWA({
-      manifest,
-      includeAssets: ["favicon.svg", "favicon.ico", "robots.txt", "apple-touch-icon.png"],
-      devOptions: {
-        enabled: true,
+      manifest: {
+        name: "Christopher Alphonse Developer",
+        short_name: "Chris Alphonse Portfolio",
+        description:
+          "Christopher Alphonse is a full-stack developer based in Boston, MA who is skilled in using technologies like JavaScript, React, and Node.js to build scalable and effective web applications. As a new developer with a passion for technology and a strong foundation in front-end and back-end development, I am eager to use my skills and knowledge to create innovative solutions for businesses and organizations. My expertise includes the MERN stack, TypeScript, Tailwind CSS, GraphQL, and MySQL. Check out my portfolio to learn more about my work and skills.",
+        display: "standalone",
+        background_color: "#000000",
+        lang: "en",
+        scope: "/",
+        start_url: "https://www.christopheralphonse.com/",
+        icons: [
+          {
+            src: "/logo_files/android-chrome-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+            purpose: "any maskable",
+          },
+          {
+            src: "/logo_files/android-chrome-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any maskable",
+          },
+        ],
+        theme_color: "#000000",
       },
     }),
   ],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
 });
