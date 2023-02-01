0208d2a19e7883fb4a65b9087adf7e193a202e4e
ChristopherAlphonse <christopheralphonse96@gmail.com>
2023-01-31 20:56:51 -0500

    mesg

--- src/components/Resume/Resume.jsx
+++ src/components/Resume/Resume.jsx
@@ -2,30 +2,14 @@ import React from 'react';
 import ResumePDF from '../../assets/sample.pdf';
 
 const Resume = () => {
+  let className = 'position:absolute;right:0; top:0; bottom:0; width:100%;';
   return (
     <div className="h-[100vh] bg-red-600">
       <div id="pdf">
-        <object
-          id="pdf_content"
-          width="100%"
-          height="1500px"
-          type="application/pdf"
-          trusted="yes"
-          application="yes"
-          title="Assembly"
-          data={`${ResumePDF}?#zoom=100&scrollbar=1&toolbar=1&navpanes=1`}
-        >
-          <embed
-            src={`${ResumePDF}`}
-            width="100%"
-            height="100%"
-            type="application/x-pdf"
-            trusted="yes"
-            application="yes"
-            title="Assembly"
-          ></embed>
-          <p>System Error - This PDF cannot be displayed, please contact Me.</p>
-        </object>
+        <iframe
+          className={className}
+          src={`${ResumePDF}?#zoom=100&scrollbar=1&toolbar=1&navpanes=1`}
+        ></iframe>
       </div>
     </div>
   );
